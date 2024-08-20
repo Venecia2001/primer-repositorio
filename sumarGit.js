1722017b973d7019cd9eefa971e7ae37d92d6c52
Jhoan Artigas <johan851artigas@gmail.com>
2024-08-20 17:46:26 -0400

    subiendo el codigo JS
    esto es un archivo que suma numeros

---
+++ sumarGit.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+let nombre = "Edmundo"
+let apellido = "Gonzales"
+=======
+let nombre = "Jhaon"
+let apellido = "gastiar"
+>>>>>>> ramita
+let PosPolitica = "Derecha"
+
+function MostrarDatos(){
+    console.log(nombre)
+    console.log(apellido)
+    console.log(PosPolitica)
+    
+    for(let i = 0; i < 10; i++){
+        console.log(`"Dios concede la victoria a la constancia"`)
+    }
+}
+
+<<<<<<< HEAD
+let variable = "7.300.000 millones por el pecho"
+=======
+>>>>>>> ramita
+console.log(MostrarDatos)
