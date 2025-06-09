9898cc90a4fa54a45ba34adf0fd49e04ff9f4b7a
Jartigas <johan851artigas@gmail.com>
2025-06-09 11:06:40 -0400

    cambiando contenido de las variables

--- sumarGit.js
+++ sumarGit.js
@@ -1,16 +1,16 @@
-let nombre = "Jhoansito"
-let apellido = "wilian"
-let PosPolitica = "Derecha"
+let nombre = "Jhoan";
+let apellido = "Barrios";
+let PosPolitica = "Ultra derecha";
 
-function MostrarDatos(){
-    console.log(nombre)
-    console.log(apellido)
-    console.log(PosPolitica)
-    
-    for(let i = 0; i < 10; i++){
-        console.log(`"Dios concede la victoria a la constancia"`)
-    }
+function MostrarDatos() {
+  console.log(nombre);
+  console.log(apellido);
+  console.log(PosPolitica);
+
+  for (let i = 0; i < 10; i++) {
+    console.log(`"Dios concede la victoria a la constancia"`);
+  }
 }
 
-let variable = "7.300.000 millones por el pecho"
-console.log(MostrarDatos)
+let variable = "7.300.000 millones";
+console.log(MostrarDatos);
